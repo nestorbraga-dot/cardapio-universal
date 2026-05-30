826a91fc0f0456e84c175364d8002d60aec2e665
nestorbraga-dot <nestorbraga738@gmail.com>
2026-05-30 17:04:51 -0300

    test: injeta chaves diretamente no codigo fonte para testar producao

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyAtgdZNhMPHydgd7CK24RkUCff6x1tNggU",
+  authDomain: "cardapio-universal.firebaseapp.com",
+  projectId: "cardapio-universal",
+  storageBucket: "cardapio-universal.firebasestorage.app",
+  messagingSenderId: "117239972317",
+  appId: "1:117239972317:web:e23efbbed0e0a2b78ab634"
 };
 
 const app = initializeApp(firebaseConfig);
