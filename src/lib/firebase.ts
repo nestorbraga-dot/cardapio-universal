705da4d05f7c567304db45690e62f05b9eacf4e8
nestorbraga-dot <nestorbraga738@gmail.com>
2026-05-30 17:35:44 -0300

    fix: desativa o emulador local e conecta ao firestore real

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSX9D0rsc4fZANbg_u60xY_Su4SxtGOVU",
-  authDomain: "cardapio-universal-um.firebaseapp.com",
+  apiKey: "AIzaSyDSX9fdvcvNbg_u60xY_Su4SxtGOVU",
+  authDomain: "://firebaseapp.com",
   projectId: "cardapio-universal-um",
   storageBucket: "cardapio-universal-um.firebasestorage.app",
-  messagingSenderId: "1097365163502",
-  appId: "1:1097365163502:web:4aa094776c7ab6066592ff"
+  messagingSenderId: "1097365cdvxf502",
+  appId: "1:1097365163502:web:4aa094776gfh6066592ff"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 export const db = getFirestore(app);
